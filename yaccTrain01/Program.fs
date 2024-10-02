4652c73d498717ca32b16c7bf22bb56d50170306
m-wachi <xxxx@yyy.zz.com>
2024-10-02 12:54:10 +0900

    initial

---
+++ yaccTrain01/Program.fs
@@ -0,0 +1,43 @@
+﻿open System.IO
+open FSharp.Text.Lexing
+
+
+let parseFile (fileName: string) =
+    use textReader = new System.IO.StreamReader(fileName)
+    let lexbuf = LexBuffer<char>.FromTextReader textReader
+
+    Parser.prog Lexer.sts_initial lexbuf
+    
+let test01 (fileName: string) =
+    use textReader = new System.IO.StreamReader(fileName)
+    let lexbuf = LexBuffer<char>.FromTextReader textReader
+
+    let lexeme = LexBuffer<_>.LexemeString
+
+    let a = Lexer.sts_initial lexbuf
+
+    //let a = lexeme lexbuf
+
+    printfn "a=%A" a
+
+    let b = Lexer.sts_initial lexbuf
+
+    printfn "b=%A" b
+
+    0
+    //Parser.prog Lexer.sts_initial lexbuf
+
+
+
+[<EntryPoint>]
+let main argv =
+
+    let testFile = Path.Combine(__SOURCE_DIRECTORY__, argv.[0])
+    let prsRes = parseFile testFile
+
+    printfn "%s" (Ast.progToStr prsRes)
+
+
+    test01 testFile |> ignore
+
+    0 // return an integer exit code
